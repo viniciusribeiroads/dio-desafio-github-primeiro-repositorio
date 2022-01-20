c1ec0c40d3b3808837ccca3f8e5c923fb2cbde8f
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-01-20 16:33:33 -0200

    Streams, collectors e APIs

--- ExerciciosJava/Streams, MethosReference e Lambda/StreamPt1.java
+++ ExerciciosJava/Streams, MethosReference e Lambda/StreamPt1.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class StreamPt1 {
 
@@ -15,7 +18,7 @@ public class StreamPt1 {
 		cursos.add(new Curso("Delphi", 3));
 		
 		//Comparando usando lambda com base no numero de alunos
-		cursos.sort(Comparator.comparing(Curso::getAlunos));
+		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
 		//Foreach usando lambda
 		cursos.forEach( s -> System.out.println(s.getNome()));
 		
@@ -32,7 +35,40 @@ public class StreamPt1 {
 		System.out.println("Somando todos alunos de cursos acima de 100 pessoas: ");
 		int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
 		System.out.println(sum);
+		
+		//Cursos me de o fluxo de todo curso, filtre quem tem mais de 100, se encontrou faça isso senao nao faça
+		Optional<Curso> optionalCurso = cursos.stream().filter( c -> c.getAlunos() >= 100)
+		.findAny();
+		//Me entregue o curso senao me entregue um null
+		Curso curso = optionalCurso.orElse(null);
+		System.out.println(curso.getNome());
+		
+		optionalCurso.ifPresent( c -> System.out.println(c.getNome()));
+		
+		//Fazendo um for usando stream
+		cursos.stream().forEach( c-> System.out.println(c.getNome()));
+		
+		//Criar uma collection a partir de um filter 
+		List<Curso> resultado = cursos.stream().filter(c -> c.getAlunos() >= 100)
+		.collect(Collectors.toList());
+		
+		//Exibindo a nova lista criada a partir do stream e filter na lista anterior
+		System.out.println("Lista criada a partir da stream e filter: ");
+		resultado.stream().forEach(e -> System.out.println(e.getNome()));
+		
+		//Utilizando mapas agora 
+		Map<String, Integer> map = cursos.stream().filter(c -> c.getAlunos() >= 100)
+				.collect(Collectors.toMap(
+						c -> c.getNome(),
+						c -> c.getAlunos()));
+		
+	
+		
+		
+		
 	}
+	
+	
 	
 	
 	public static class Curso {
